e25709b6b1e9226522fa8c3ebce1ca287fe526ef
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-04-28 14:06:10 -0400

    Add files via upload

--- mybot.js
+++ mybot.js
@@ -735,21 +735,25 @@ V`).then(() => {
 	if(command === "setprefix"){
 	  if(!message.member.hasPermission("ADMINISTRATOR")) {
 		  return message.reply("sorry, you don't have permissions to use this.");
-	  }
-	  mongo.connect(ServerURL, function(err, db) {
-		var dbo = db.db("servers");
-		var query = { "serverID": message.guild.id };
-		dbo.collection("servers").findOne(query, function(err, result ) {
-			if(err) throw err;
-			var t = defaultServer;
-			t.serverID = message.guild.id;
-			t.prefix = args[0].toString();
-			dbo.collection("servers").update(query, t, function (err, res) {
+	  
+	  if(args[0].length > 10){
+		  message.reply("your prefix can't be longer than 10 characters");
+	  } else {
+		mongo.connect(ServerURL, function(err, db) {
+			var dbo = db.db("servers");
+			var query = { "serverID": message.guild.id };
+			dbo.collection("servers").findOne(query, function(err, result ) {
 				if(err) throw err;
-				message.reply("guild prefix updated to `" + args[0] + "`");
+				var t = defaultServer;
+				t.serverID = message.guild.id;
+				t.prefix = args[0].toString();
+				dbo.collection("servers").update(query, t, function (err, res) {
+					if(err) throw err;
+					message.reply("guild prefix updated to `" + args[0] + "`");
+				});
 			});
 		});
-	  });
+	  }
   }
   
 	if(command === "help") {
