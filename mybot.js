62d5ea4af8a167f380b7923a64f8e857f1e55ab8
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-04-29 14:09:22 -0400

    Add files via upload

--- mybot.js
+++ mybot.js
@@ -993,7 +993,10 @@ function rotateFunction (message, degrees, im) {
 			//catch(err);
 		} else {
 			image.rotate(parseFloat(degrees), true, function(err){
-				if(err) message.reply("you've done something wrong! Are you sure you did `e!rotate [degrees] [link/user]`?").then(return);
+				if(err) { 
+					message.reply("you've done something wrong! Are you sure you did `e!rotate [degrees] [link/user]`?");
+					return;
+				}
 				image.write("/app/tempPic.png", function (err) {
 					if(err) throw err;
 					message.channel.send("", { files: ["/app/tempPic.png"]}).then(message.channel.stopTyping());
