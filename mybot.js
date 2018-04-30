68e38f2a29d13a6e888fda3a1a1a7d6e9b760969
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-04-29 21:53:18 -0400

    Balance now saves as .png
    Change was made to see if it speeds the message up. I guess we'll see...

--- mybot.js
+++ mybot.js
@@ -948,8 +948,8 @@ function makeProfile (mes, money, xp, level, tag) {
 							if(err) throw err;
 							image.print(font,36,410, `$${money}`).getBuffer(Jimp.MIME_JPEG, function (err, img) {
 								if(err) throw err;
-								image.scale(0.35).write("/app/tempBal.jpg");
-								mes.channel.send("", { files: ["/app/tempBal.jpg"]});
+								image.scale(0.35).write("/app/tempBal.png");
+								mes.channel.send("", { files: ["/app/tempBal.png"]});
 								mes.channel.stopTyping();
 							});
 						});
